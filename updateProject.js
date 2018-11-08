dc5050552e359a0cc4b194ac37c5ba34d067f2ba
Pariksit <pd3g16@soton.ac.uk>
2018-11-08 22:17:33 +0000

    Updated API to allow the use of updating projects as well as getting it.
    API allows the retrieval of projects, lists them by userId, assigns a unique projectId instead of setting projectId as title. Updates the project upon clicking save when viewing a project. All changes are committed to Dynamodb simultaneously.

---
+++ updateProject.js
@@ -0,0 +1,45 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+import uuid from "uuid";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  
+  const params = {
+    TableName: "projects",
+    Key: {
+        projectId: event.pathParameters.id    
+    },
+    
+    UpdateExpression: "SET content = :content, title = :title, projectStatus = :projectStatus, developers = :developers",
+    ExpressionAttributeValues: {
+      ":content": data.content || null,
+      ":title": data.title || null,
+      ":projectStatus": data.projectStatus || null,
+      ":developers": data.developers || null,
+    },
+    // 'ReturnValues' specifies if and how to return the item's attributes,
+    // where ALL_NEW returns all attributes of the item after the update; you
+    // can inspect 'result' below to see how it works with different settings
+    
+    ReturnValues: "ALL_NEW"
+
+    /*Item: {
+      projectId: uuid.v1(),
+      title: data.title,
+      mgmtId: data.mgmtId, 
+      content: data.content,
+      createdAt: Date.now(),
+      projectStatus: data.projectStatus,
+      developers:[]
+    }*/
+  };
+
+  try {
+    await dynamoDbLib.call("update", params);
+    return success(params.Item);
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
+}
